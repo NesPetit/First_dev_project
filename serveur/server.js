8700f0e672142e63df07d533ef00ecac5d340b38
Alec <alec.92@hotmail.fr>
2020-03-23 22:22:38 +0100

    v3

--- serveur/server.js
+++ serveur/server.js
@@ -1,11 +1,12 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', express.static(path.resolve(_dirname, '../client/dist')))
+app.use(express.static(path.resolve(__dirname, '../client/dist')))
 
 app.use('*', (req, res) => {
-    res.status(404).res.sendFile(path.resolve(_dirname,'../client/dist/404.html'))
+    res.status(404).sendFile(path.resolve(__dirname,'../client/dist/404.html'))
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`You can go to http://localhost:${port}`))
